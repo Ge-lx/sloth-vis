81eda2a1dd2dda6ead60125af5c553cc37dbcca1
Falk Ebert <f4lkebert@gmail.com>
2020-12-30 13:55:22 +0100

    Pi Changes

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ configurations = {
 		# GUI Configuration
 		# ----------------------------------
 		# Whether or not to display a PyQtGraph GUI plot of visualization
-		'USE_GUI': True,
+		'USE_GUI': False,
 		# Whether to display debug information
 		'DEBUG': True,
 		# Target GUI framerate. Will warn when this can't be met.
@@ -23,8 +23,8 @@ configurations = {
 		# Hardware sample rate
 		'SAMPLE_RATE': 44100,
 		# Alsa Device names (typically hw:0,0 or something like that)
-		'ALSA_SOURCE': 'hw:3,1',
-		'ALSA_SINK': 'pulse',
+		'ALSA_SOURCE': 'hw:1,1',
+		'ALSA_SINK': 'rate_convert',
 
 		# LED Output
 		# ----------------------------------
@@ -46,12 +46,12 @@ configurations = {
 		# Frequencies below this value will be removed during audio processing
 		'MIN_FREQUENCY': 20,
 		# Frequencies above this value will be removed during audio processing
-		'MAX_FREQUENCY': 20000,
+		'MAX_FREQUENCY': 10000,
 		# Number of frequency bins to use when transforming audio to frequency domain
-		'FFT_N_BINS': 150,
+		'FFT_N_BINS': 100,
 		#Length (ms) of the rolling audio window to be used. Will be adjusted to
 		# improve fft performance.
-		'FFT_WINDOW_LENGTH': 50
+		'FFT_WINDOW_LENGTH': 100
 	}
 }
 
@@ -71,21 +71,15 @@ def visualizations(config):
 	    zeros = np.zeros(config.N_PIXELS);
 	    return np.array([zeros, zeros, zeros, clipped]);
 
-	def visualize_spectrum(spectrum, _):
-	    interpolated = dsp.interpolate(spectrum, config.N_PIXELS)
-	    pixels = np.array([
-	        # np.clip(1*np.log(interpolated*10), 0, 1),
-	        # np.clip(0.3*np.log(interpolated*10), 0, 1),
-	        # np.clip(0.3 * interpolated, 0, 1)
-	        # np.tile(0, config.N_PIXELS),
-	        # np.tile(0, config.N_PIXELS),
-	        # np.clip(0.3 * interpolated, 0, 1),
-	        interpolated,
-	        interpolated,
-	        interpolated,
-	        interpolated
-	    ])
-	    return np.clip(pixels, 0, 1) * 255;
+		def visualize_spectrum(spectrum, _):
+			interpolated = dsp.interpolate(spectrum, config.N_PIXELS)
+			pixels = np.array([
+				np.clip(1*np.log(interpolated*10), 0, 1),
+				np.clip(0.3*np.log(interpolated*10), 0, 1),
+				np.tile(0, config.N_PIXELS),
+				np.clip(0.3 * interpolated, 0, 1),
+			])
+		return pixels * 255;
 
 	return {
 		'waveform': visualize_waveform,
